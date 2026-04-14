631d9aa485f9c6146d28917be7a15cf64bd200bc
Maheshwarichervirala <maheshwarichervirala@gmail.com>
2026-04-14 17:42:26 +0530

    Fixed secrets and updated project

--- server/server.js
+++ server/server.js
@@ -6,10 +6,11 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
-// ✅ Use NON-SRV connection string
-mongoose.connect("mongodb://Mahi:Prajwal2418@ac-dmnkpsz-shard-00-00.w7g0xtv.mongodb.net:27017,ac-dmnkpsz-shard-00-01.w7g0xtv.mongodb.net:27017,ac-dmnkpsz-shard-00-02.w7g0xtv.mongodb.net:27017/Videoedit?ssl=true&replicaSet=atlas-n23rz1-shard-0&authSource=admin&retryWrites=true&w=majority")
-.then(() => console.log("MongoDB Connected"))
-.catch(err => console.log("DB Error:", err));
+require('dotenv').config();
+
+mongoose.connect(process.env.MONGO_URI)
+  .then(() => console.log("DB Connected"))
+  .catch(err => console.log(err));
 
 const Editor = mongoose.model("Editor", {
     name: String,
